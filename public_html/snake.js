d3f49b5b5f5b21da71628bbf0cf1442f6b3c2272
Ricardo Harris <ricardoharris@ymail.com>
2014-10-03 16:42:31 -0700

    3rd commit

--- public_html/snake.js
+++ public_html/snake.js
@@ -1,3 +1,25 @@
+var snake;
 
+var context;
+var screenWidth;
+var screenHeight;
 
+function gameInitialize(){
+    var canvas= document.getElementById("gameScreen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHieght= window.innerHeight;
+    
+    canvas.width= screenWidth;
+    canvas.height= screenHeight;
+}
 
+function gameLoop(){
+    
+}
+
+function gamedraw(){
+    context.fillStyle = "rgb(222, 250, 250)";
+    context.fillRect (0,0, screenWidth, screenHeight);
+}
