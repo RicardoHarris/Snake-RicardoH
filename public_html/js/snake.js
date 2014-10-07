0734a8563edf4548c016a39a830e12cf45b792ad
Ricardo Harris <ricardoharris@ymail.com>
2014-10-06 19:56:35 -0700

    4th commit (I think) didn't push in class WOOPS. Finished milestone one, there is an error that I need Mr.Larrimore's help on.

---
+++ public_html/js/snake.js
@@ -0,0 +1,67 @@
+var snake;
+var snakeLenth;
+var snakeSize;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+gameInitialize();
+snakeInitialize();
+setInterval(gameLoop, 1000/30);
+
+
+function gameInitialize(){
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+}
+
+function gameLoop(){
+    snakeDraw();
+    snakeUpdate();
+    snakeDraw();
+}
+
+function gameDraw(){
+    context.fillStyle = "rgb(255, 255, 255)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize(){
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 35;
+    
+    for(var index = snakeLength; index >= 0; index--){
+        snake.push( {
+            x: index,
+            y: 0
+        });
+        
+    }
+}
+
+function snakeDraw(){
+    for(var index = 0; index < snake.length; index++){
+        context.fillStyle = "pink";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
+}
+
+function snakeUpdate(){
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    
+    snakeHeadX++;
+    
+    var snakeTail = snake.pop();
+    snakeTail.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
+}
